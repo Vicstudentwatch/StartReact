3f9d5e2219b4776b2e86a006a2bbdb28b3242845
Victor Muhita <wangombevictor5@gmail.com>
2025-06-14 17:57:51 +0300

    add welcome message

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import WelcomeMessage from './WelcomeMessage'
 
 const MainContent = () => {
   return (
     <div>
         <h2>Main Content</h2>
+        <WelcomeMessage/>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus voluptatem harum dolorem ex dignissimos alias vero, ea itaque porro neque voluptatibus cumque voluptatum nemo laudantium sed. Consequuntur fuga esse quos.
         Ut, nam quas deserunt quos fuga illo assumenda officia eligendi eaque repudiandae vero a aliquid hic omnis natus animi! Fugit similique voluptatum maxime natus, iste libero perferendis quasi dicta fugiat.
         Nulla quibusdam tenetur aut magnam porro laborum saepe perspiciatis laboriosam impedit, dolore possimus dicta animi distinctio provident placeat. Tempore, iste sapiente! Provident voluptatem laudantium fugiat eos, totam eum id consequuntur.

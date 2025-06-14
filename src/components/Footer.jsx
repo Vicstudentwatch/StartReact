2d8da0e7a4f344e63c09afa4635871fcae6d3f09
Victor Muhita <wangombevictor5@gmail.com>
2025-06-14 17:49:45 +0300

    start React

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <p>@2024 my Website</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <div>
+        <h2>Main Content</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus voluptatem harum dolorem ex dignissimos alias vero, ea itaque porro neque voluptatibus cumque voluptatum nemo laudantium sed. Consequuntur fuga esse quos.
+        Ut, nam quas deserunt quos fuga illo assumenda officia eligendi eaque repudiandae vero a aliquid hic omnis natus animi! Fugit similique voluptatum maxime natus, iste libero perferendis quasi dicta fugiat.
+        Nulla quibusdam tenetur aut magnam porro laborum saepe perspiciatis laboriosam impedit, dolore possimus dicta animi distinctio provident placeat. Tempore, iste sapiente! Provident voluptatem laudantium fugiat eos, totam eum id consequuntur.
+        </p>
+    </div>
+  )
+}
+
+export default MainContent

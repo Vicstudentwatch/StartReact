963a932fa6b6f76f91dad1352bc4e11a469f20b2
Victor Muhita <wangombevictor5@gmail.com>
2025-06-14 18:07:39 +0300

    JSX rules

---
+++ src/components/JsxRules.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const JsxRules = () => {
+  return (
+    <div>
+        <h1>JsxRules</h1>
+        <p>
+            <ul>
+            <li>JSX must return <strong>One Single Element</strong></li>
+            <li>Jsx Element must be <strong>properly Closed</strong></li>
+            <li>JSX attributes are <strong>written using CamelCase </strong></li>
+            </ul>
+        </p>
+    </div>
+  )
+}
+
+export default JsxRules

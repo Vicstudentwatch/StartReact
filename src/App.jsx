02f21816e2f44b2519985e4a4722f94887379d4e
Victor Muhita <wangombevictor5@gmail.com>
2025-06-24 11:28:42 +0300

    explore useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Counter from "./components/Counter"
 import CounterReducer from "./components/CounterReducer"
 
 const App = () => {
   return (
     <div>
       <CounterReducer/>
+      <Counter/>
     </div>
     
   )
--- src/components/CounterReducer.jsx
+++ src/components/CounterReducer.jsx
@@ -3,16 +3,19 @@ const reducer = (state,action) =>{
     switch(action.type){
         case "increment":
             return {count:state.count+1}
+        case "decrement":
+            return {count:state.count-1}
     }
 }
 
 const CounterReducer = () => {
     const [state,dispatch] = useReducer(reducer,{count:0})
   return (
-    <>
-    <div>{state.count}</div>
+    <p>
+    <button onClick={()=>dispatch({type:"decrement"})}>-</button>
+    <span>{state.count}</span>
     <button onClick={()=>dispatch({type:"increment"})}>+</button>
-    </>
+    </p>
     )}
 
 export default CounterReducer
